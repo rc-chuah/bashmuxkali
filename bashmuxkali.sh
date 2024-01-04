cbd14b93d04b88d5bb8bf5a4de580d2309156ea7
RC Chuah <44928288+rc-chuah@users.noreply.github.com>
2024-01-04 17:07:24 +0800

    Update bashmuxkali.sh

--- bashmuxkali.sh
+++ bashmuxkali.sh
@@ -5,7 +5,22 @@
 # Script Structure
 # ================
 
-# Show Usage Via Command Line Arguments
+# Install Kali Nethunter In Termux Full Version Function
+function full() {
+  bash -c "cd ${HOME} && $(curl -fsSL https://bit.ly/install-nethunter-full-termux) && rm -rf ${HOME}/kalifs-armhf-full.tar.xz && rm -rf ${HOME}/kalifs-arm64-full.tar.xz && rm -rf ${HOME}/kalifs-armhf-full.sha512sum && rm -rf ${HOME}/kalifs-arm64-full.sha512sum"
+}
+
+# Install Kali Nethunter In Termux Minimal Version Function
+function minimal() {
+  bash -c "cd ${HOME} && $(curl -fsSL https://bit.ly/install-nethunter-minimal-termux) && rm -rf ${HOME}/kalifs-armhf-minimal.tar.xz && rm -rf ${HOME}/kalifs-arm64-minimal.tar.xz && rm -rf ${HOME}/kalifs-armhf-minimal.sha512sum && rm -rf ${HOME}/kalifs-arm64-minimal.sha512sum"
+}
+
+# Install Kali Nethunter In Termux Nano Version Function
+function nano() {
+  bash -c "cd ${HOME} && $(curl -fsSL https://bit.ly/install-nethunter-nano-termux) && rm -rf ${HOME}/kalifs-armhf-nano.tar.xz && rm -rf ${HOME}/kalifs-arm64-nano.tar.xz && rm -rf ${HOME}/kalifs-armhf-nano.sha512sum && rm -rf ${HOME}/kalifs-arm64-nano.sha512sum"
+}
+
+# Show Usage Via Command Line Arguments Function
 function usage() {
   echo "usage: $0 [-f] [-m] [-n] [-u] [-h]"
   echo ""
@@ -25,26 +40,14 @@ function usage() {
   exit
 }
 
-# ===================
-# Script Funtionality
-# ===================
-
-# Command Line Arguments
-function full() {
-  bash -c "cd ${HOME} && $(curl -fsSL https://bit.ly/install-nethunter-full-termux) && rm -rf ${HOME}/kalifs-armhf-full.tar.xz && rm -rf ${HOME}/kalifs-arm64-full.tar.xz && rm -rf ${HOME}/kalifs-armhf-full.sha512sum && rm -rf ${HOME}/kalifs-arm64-full.sha512sum"
-}
-
-function minimal() {
-  bash -c "cd ${HOME} && $(curl -fsSL https://bit.ly/install-nethunter-minimal-termux) && rm -rf ${HOME}/kalifs-armhf-minimal.tar.xz && rm -rf ${HOME}/kalifs-arm64-minimal.tar.xz && rm -rf ${HOME}/kalifs-armhf-minimal.sha512sum && rm -rf ${HOME}/kalifs-arm64-minimal.sha512sum"
-}
-
-function nano() {
-  bash -c "cd ${HOME} && $(curl -fsSL https://bit.ly/install-nethunter-nano-termux) && rm -rf ${HOME}/kalifs-armhf-nano.tar.xz && rm -rf ${HOME}/kalifs-arm64-nano.tar.xz && rm -rf ${HOME}/kalifs-armhf-nano.sha512sum && rm -rf ${HOME}/kalifs-arm64-nano.sha512sum"
-}
-
+# Uninstall Kali Nethunter In Termux Function
 function uninstall() {
   bash -c "rm -rf ${HOME}/kali-arm64 && rm -rf ${HOME}/kali-armhf && rm -rf ${PREFIX}/bin/nh && rm -rf ${PREFIX}/bin/nethunter && sleep 1 && echo [+] Successfully Uninstalled ..."
 }
+
+# ===================
+# Script Funtionality
+# ===================
 
 # If No Arguments Provided, Display Usage Information
 [[ $# -eq 0 ]] && usage
@@ -52,6 +55,7 @@ function uninstall() {
 # Process Command Line Arguments
 if [[ $@ ]]; then
   while [[ "$1" != "" ]]; do
+    # Command Line Arguments
     case $1 in
       -f|--full)
         full
